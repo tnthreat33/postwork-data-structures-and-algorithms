0ecd7c7c6a984af1a57f464e2f45d6369bdaa770
Rachel Hamby <tnthreat33@gmail.com>
2023-11-01 20:12:37 -0400

    done

--- 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
+++ 01-week-1--starter-algorithms/01-day-2--find-first-duplicate/javascript/find_first_duplicate.js
@@ -14,6 +14,39 @@ if (require.main === module) {
 }
 
 module.exports = findFirstDuplicate;
+// Given an Array, find the first duplicate value that occurs. If there are no
+// duplicates, return -1.
 
+// ```js
+// Input: [2, 1, 3, 3, 2]
+// Output: 3
+
+// Input: [1, 2, 3, 4]
+// Output: -1
+// ```
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+//test cases
+//input: []
+//output: -1
+//input: [1,1,2,2]
+//output: 1
+
+//function dup takes an array as an argument
+//create a new array 
+//map through array for each number check if it is in the new array 
+//if it isn't push the number to the array
+//if it is then end the loop and return the number
+
+function dub (arr){
+  uniqueArr = new Set()
+
+  for (const n in arr){
+    if (uniqueArr.has(n)){
+      return n;
+    } else{
+      uniqueArr.push(n)
+    }
+  }
+  return -1 
+}
