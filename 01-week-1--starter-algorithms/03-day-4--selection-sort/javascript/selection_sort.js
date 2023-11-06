37c51d6a5031ecef177d3d66eeb986c0df07746f
Rachel Hamby <tnthreat33@gmail.com>
2023-11-05 20:34:48 -0500

    redo

--- 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
+++ 01-week-1--starter-algorithms/03-day-4--selection-sort/javascript/selection_sort.js
@@ -1,5 +1,28 @@
 function selectionSort(arr) {
-  // type your code here
+  // Loop through the entire array
+  for (let i = 0; i < arr.length; i++) {
+    // Assume the current element is the minimum
+    let minIndex = i;
+
+    // Find the index of the minimum element in the remaining unsorted portion of the array
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // Swap the minimum element with the current element
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+
+  // The array is now sorted
+  return arr;
+
+  
 }
 
 if (require.main === module) {
@@ -19,5 +42,10 @@ if (require.main === module) {
 
 module.exports = selectionSort;
 
+//problem
+  //take an array and check each number in the array to put them in order if there are 2 of the same for the one that shows up first first. 
 // Please add your pseudocode to this file
+  //loop through the array for the number of time of the number of index
+  //create a new arr that houses the sorted array
+
 // And a written explanation of your solution
